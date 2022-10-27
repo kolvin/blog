600995761b195c9d1fd647a6a8f3f832bb638b6a
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-10-27 22:26:16 +0100

    fix: base seo image path fix...
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -3,7 +3,7 @@ const siteMeta = {
   title: "Kolv.in",
   description: "Personal site of an average engineer. I hate computers",
   twitterAuthor: "@k0lvin",
-  imageUrl: "/images/milo/abstract.png",
+  imageUrl: "/images/misc/abstract.png",
   lang: "en-GB",
   ogLocale: "en_GB",
   themeColorLight: "#fafafa",
